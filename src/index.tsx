f1ba817df9b293a70e7365e9049ec1475dd1bc34
makeev <makeevi2004@mail.ru>
2026-01-08 19:04:21 +0300

    Преобразован ArticleParamsForm для управления состоянием статьи и динамического применения изменений. Обновлены стили для улучшения оформления и отрегулирована логика выбора параметров радиогруппы.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -12,20 +15,31 @@ import styles from './styles/index.module.scss';
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
+const initialArticleState: ArticleStateType = defaultArticleState;
+
 const App = () => {
+	const [appliedArticleState, setAppliedArticleState] =
+		useState<ArticleStateType>(initialArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appliedArticleState.fontFamilyOption.value,
+					'--font-size': appliedArticleState.fontSizeOption.value,
+					'--font-color': appliedArticleState.fontColor.value,
+					'--container-width': appliedArticleState.contentWidth.value,
+					'--bg-color': appliedArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				articleState={{
+					applied: appliedArticleState,
+					initial: initialArticleState,
+					apply: setAppliedArticleState,
+				}}
+			/>
 			<Article />
 		</main>
 	);
